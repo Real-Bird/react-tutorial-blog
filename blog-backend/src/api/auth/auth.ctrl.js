a2a77b13e936ec168bf21592579eaaa2bb0e24cd
Real-Bird <marbil815@gmail.com>
2022-11-03 17:57:43 +0900

    22-11-03 23 fin

--- blog-backend/src/api/auth/auth.ctrl.js
+++ blog-backend/src/api/auth/auth.ctrl.js
@@ -28,10 +28,17 @@ export const register = async (ctx) => {
     await user.save();
 
     ctx.body = user.serialize();
+
+    const token = user.generateToken();
+    ctx.cookies.set('access_token', token, {
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+      httpOnly: true,
+    });
   } catch (e) {
     ctx.throw(500, e);
   }
 };
+
 export const login = async (ctx) => {
   const { username, password } = ctx.request.body;
   if (!username || !password) {
@@ -52,9 +59,27 @@ export const login = async (ctx) => {
       return;
     }
     ctx.body = user.serialize();
+
+    const token = user.generateToken();
+    ctx.cookies.set('access_token', token, {
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+      httpOnly: true,
+    });
   } catch (e) {
     ctx.throw(500, e);
   }
 };
-export const check = async (ctx) => {};
-export const logout = async (ctx) => {};
+
+export const check = async (ctx) => {
+  const { user } = ctx.state;
+  if (!user) {
+    ctx.status = 401;
+    return;
+  }
+  ctx.body = user;
+};
+
+export const logout = async (ctx) => {
+  ctx.cookies.set('access-token');
+  ctx.status = 204;
+};
--- blog-backend/src/api/posts/posts.ctrl.js
+++ blog-backend/src/api/posts/posts.ctrl.js
@@ -5,10 +5,29 @@ const Joi = require('joi');
 
 const { ObjectId } = mongoose.Types;
 
-export const checkObjectId = (ctx, next) => {
+export const getPostById = async (ctx, next) => {
   const { id } = ctx.params;
   if (!ObjectId.isValid(id)) {
     ctx.status = 400;
+    return;
+  }
+  try {
+    const post = await Post.findById(id);
+    if (!post) {
+      ctx.status = 404;
+      return;
+    }
+    ctx.state.post = post;
+    return next();
+  } catch (e) {
+    ctx.throw(500, e);
+  }
+};
+
+export const checkOwnPost = (ctx, next) => {
+  const { user, post } = ctx.state;
+  if (post.user._id !== user._id) {
+    ctx.status = 403;
     return;
   }
   return next();
@@ -22,7 +41,6 @@ export const checkObjectId = (ctx, next) => {
  *  tags: ['태그1', '태그2']
  * }
  */
-
 export const write = async (ctx) => {
   const schema = Joi.object().keys({
     title: Joi.string().required(),
@@ -42,6 +60,7 @@ export const write = async (ctx) => {
     title,
     body,
     tags,
+    user: ctx.state.user,
   });
   try {
     await post.save();
@@ -52,24 +71,29 @@ export const write = async (ctx) => {
 };
 
 /**
- * GET /api/posts
+ * GET /api/posts?username=&tag=&page=
  */
 export const list = async (ctx) => {
   const page = parseInt(ctx.query.page || '1', 10);
-
   if (page < 1) {
     ctx.status = 400;
     return;
   }
 
+  const { username, tag } = ctx.query;
+  const query = {
+    ...(username ? { 'user.username': username } : {}),
+    ...(tag ? { tags: tag } : {}),
+  };
+
   try {
-    const posts = await Post.find()
+    const posts = await Post.find(query)
       .sort({ _id: -1 })
       .limit(10)
       .skip((page - 1) * 10)
       .lean()
       .exec();
-    const postCount = await Post.countDocuments().exec();
+    const postCount = await Post.countDocuments(query).exec();
     ctx.set('Last-Page', Math.ceil(postCount / 10));
     ctx.body = posts.map((post) => ({
       ...post,
@@ -85,17 +109,7 @@ export const list = async (ctx) => {
  * GET /api/posts/:id
  */
 export const read = async (ctx) => {
-  const { id } = ctx.params;
-  try {
-    const post = await Post.findById(id).exec();
-    if (!post) {
-      ctx.status = 404;
-      return;
-    }
-    ctx.body = post;
-  } catch (e) {
-    ctx.throw(500, e);
-  }
+  ctx.body = ctx.state.post;
 };
 
 /**
